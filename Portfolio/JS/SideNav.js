227731c2e4e65227c96cfa430a91f4597356ba98
RheCreate <75132698+RheCreate@users.noreply.github.com>
2021-11-01 12:34:36 -0500

    Create SideNav.js

---
+++ Portfolio/JS/SideNav.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "333px";
+    document.getElementById("bodymain").style.marginRight = "333px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("bodymain").style.marginRight = "0";
+  }
